afb7ed27e4378801532ab2f6e3ee0ab42b6a79d7
Thymck <thymucak@gmail.com>
2025-06-02 20:42:16 +0200

    Fix fetch paths to use relative URLs for GitHub Pages

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-fetch("../headerfooter/header.html")
+// Eğer app.js hem index.html hem contact/index.html gibi farklı dizinlerde kullanılıyorsa,
+// fetch yollarını dinamik yapabiliriz.
+
+const basePath = window.location.pathname.includes("/contact/") ? "../" : "";
+
+fetch(basePath + "headerfooter/header.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
-fetch("../headerfooter/header.html")
+fetch(basePath + "headerfooter/footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
