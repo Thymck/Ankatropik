02a7ccdd7185c612781562a0bf0d8daa86ce6c57
Thymck <thymucak@gmail.com>
2025-06-02 19:00:26 +0200

    Rename local folder to match GitHub repo name

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-fetch("/2025/Anka-tropik-main/headerfooter/header.html")
+fetch("/2025/Ankatropik/headerfooter/header.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
-fetch("/2025/Anka-tropik-main/headerfooter/footer.html")
+fetch("/2025/Ankatropik/headerfooter/footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
