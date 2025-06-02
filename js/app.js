db1e585adc1b4f2ff7a5a3e7ee9d722115a1291f
Thymck <thymucak@gmail.com>
2025-06-02 21:08:29 +0200

    Fix fetch paths to use relative URLs for GitHub Pages

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-// Sayfanın kökten itibaren derinliğini al
-const pathDepth =
-  window.location.pathname.replace(/\/$/, "").split("/").length - 3;
-
-// Örn. index.html için: 0 → ./
-//      contact/index.html için: 1 → ../
-//      product/detail/index.html için: 2 → ../../
-const basePath = pathDepth > 0 ? "../".repeat(pathDepth) : "./";
-
-// Header yükle
-fetch(basePath + "headerfooter/header.html")
+fetch("headerfooter/header.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
-// Footer yükle
-fetch(basePath + "headerfooter/footer.html")
+fetch("headerfooter/footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
