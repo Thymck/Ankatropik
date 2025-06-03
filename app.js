66fa6c71efe065bd4f961770bd7b81cd99e5e41d
Thymck <thymucak@gmail.com>
2025-06-03 12:17:09 +0200

    Fix relative paths and update header/footer for GitHub Pages

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-fetch("/headerfooter/header.html")
+let basePath =
+  window.location.pathname.includes("/contact/") ||
+  window.location.pathname.includes("/product/")
+    ? "../headerfooter/"
+    : "headerfooter/";
+
+fetch(basePath + "header.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
-fetch("/headerfooter/footer.html")
+fetch(basePath + "footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
